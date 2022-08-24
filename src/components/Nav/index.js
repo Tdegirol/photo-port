c3e66d8ef5dfb53d2c9685955cf61fa76d125f4f
TJ <tdegirol@gmail.com>
2022-08-24 18:36:56 -0400

    set up header & nav

---
+++ src/components/Nav/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+function Nav(){
+
+    function categorySelected(name) {
+        console.log(`${name} clicked`);
+    }
+
+    const categories = [
+        {
+            name: 'Commercial',
+            description:'Photos of grocery stores, food trucks, and other commercial products',
+        },
+        { name: 'Portraits', description: 'Portaits of people in my life' },
+        { name: 'Food', description: 'Delicious delicacies' },
+        {
+            name: 'Landscape',
+            description: 'Fields, farmhouses, waterfalls, and the beauty of nature',
+        },
+    ];
+
+    return(
+        <header>
+            <h2>
+                <a href='/'>
+                    <span role='img' aria-label='camera'></span> Oh snap!
+                </a>
+            </h2>
+            <nav>
+                <ul className='flex-row'>
+                    <li className='mx-2'>
+                        <a href='#about'>
+                            About me
+                        </a>
+                    </li>
+                    <li>
+                        <span>Contact</span>
+                    </li>
+                    {categories.map((category) => (
+                        <li
+                            className='mx-1'
+                            key={category.name}
+                        >
+                            <span onClick={categorySelected(category.name)}>
+                                {category.name}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Nav;
